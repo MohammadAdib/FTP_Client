8ebf69a2e5e37cf09b7cf288587aa462750f1c1c
Mohammad Adib <m.a.adib96@gmail.com>
2013-04-14 19:01:58 -0700

    Commit
    Made open source

---
+++ FTP Client/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace FTP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hostBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.portNUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dirBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portNUD)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hostBox
+            // 
+            this.hostBox.Location = new System.Drawing.Point(45, 20);
+            this.hostBox.Name = "hostBox";
+            this.hostBox.Size = new System.Drawing.Size(100, 20);
+            this.hostBox.TabIndex = 0;
+            this.hostBox.Text = "24.16.159.83";
+            this.hostBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.portNUD);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.hostBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 48);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Socket Configuration";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // portNUD
+            // 
+            this.portNUD.Location = new System.Drawing.Point(186, 19);
+            this.portNUD.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.portNUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.portNUD.Name = "portNUD";
+            this.portNUD.Size = new System.Drawing.Size(61, 20);
+            this.portNUD.TabIndex = 3;
+            this.portNUD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.portNUD.Value = new decimal(new int[] {
+            21,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Host:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Remote Directory:";
+            // 
+            // dirBox
+            // 
+            this.dirBox.Location = new System.Drawing.Point(107, 113);
+            this.dirBox.Name = "dirBox";
+            this.dirBox.Size = new System.Drawing.Size(160, 20);
+            this.dirBox.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.White;
+            this.textBox.Location = new System.Drawing.Point(12, 140);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(335, 197);
+            this.textBox.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.passBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.userBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(13, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 47);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Credentials";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Username:";
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(71, 19);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(100, 20);
+            this.userBox.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password:";
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(240, 20);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(87, 20);
+            this.passBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 349);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dirBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "FTP Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portNUD)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hostBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dirBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.NumericUpDown portNUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FTP Client/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace FTP_Client
+{
+    public partial class Form1 : Form
+    {
+        FTPClient ftp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ftp = new FTPClient(hostBox.Text, userBox.Text, passBox.Text);
+                ftp.RemotePort = (int)portNUD.Value;
+                listDirs();
+            }
+            catch
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void listDirs()
+        {
+            string[] list = ftp.GetFileList();
+            String lines = "";
+            foreach (string s in list)
+            {
+                lines += s + "\r\n";
+            }
+            textBox.Text = lines;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (ftp != null)
+            {
+                ftp = new FTPClient(hostBox.Text, "Dropbox", "cisco");
+                ftp.RemotePath = dirBox.Text;
+                listDirs();
+            }
+        }
+    }
+}
